e8073b0f1490cf8025fc4aed3cb84c3801cb9e26
Pravin Karthick <46345354+Pravin93-Murugesan@users.noreply.github.com>
2020-01-14 15:46:15 +0100

    Update API_Weather.py

--- API_Weather.py
+++ API_Weather.py
@@ -1,9 +1,10 @@
 # Open Weather
+# Create a free account to make API requests
  
 import requests
 
 baseURL = 'https://api.openweathermap.org/data/2.5/forecast'
 # Ditionary key APPID is specific to this website
-parameters = {'APPID':'87c5e7f6eb8d0f0989667f8c9ea58785', 'q':'Aalborg, DK'} 
+parameters = {'APPID':'enter your key', 'q':'Aalborg, DK'} 
 response = requests.get(baseURL, params=parameters)
 print(response.content)
