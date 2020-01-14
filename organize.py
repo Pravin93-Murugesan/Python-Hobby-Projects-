41eae7c30ce85050ee7e87c088abf6f321fcdfcb
Pravin Karthick <46345354+Pravin93-Murugesan@users.noreply.github.com>
2020-01-14 15:38:54 +0100

    Add files via upload

---
+++ organize.py
@@ -0,0 +1,52 @@
+import os
+from pathlib import Path
+
+# Change working directly (temporary)
+os.chdir('C:\\Users\\RAM\\Desktop\\Python\\Organize\\Ex_Files_Python_Automation\\Exercise Files\\OrganizeMe')
+
+# Directory Dictionary
+SUBDIRECTORIES = {
+    "HTML": [".html5", ".html", ".htm", ".xhtml"],
+    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
+               ".heif", ".psd"],
+    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
+               ".qt", ".mpg", ".mpeg", ".3gp", ".mkv"],
+    "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
+                  ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
+                  ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
+                  "pptx"],
+    "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
+                 ".dmg", ".rar", ".xar", ".zip"],
+    "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
+              ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
+    "PLAINTEXT": [".txt", ".in", ".out"],
+    "PDF": [".pdf"],
+    "PYTHON": [".py"],
+    "XML": [".xml"],
+    "EXE": [".exe"],
+    "SHELL": [".sh"]
+}
+
+# Test an example
+def pickdirectory(value):
+    for category, suffixes in SUBDIRECTORIES.items():
+        for suffix in suffixes:
+            if suffix == value:
+                return category
+    return 'MISC'
+#print(pickdirectory('.mp3'))
+
+# Organize each file in a directory
+def organizedirectory():
+    for item in os.scandir():
+        if item.is_dir():
+            continue
+        filepath = Path(item)
+        filetype = filepath.suffix.lower()
+        directory = pickdirectory(filetype)
+        dirpath = Path(directory)
+        if dirpath.is_dir() != True:
+            dirpath.mkdir()
+        filepath.rename(dirpath.joinpath(filepath))
+
+organizedirectory()
